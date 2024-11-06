2f7d2659f4f08c6884ce80414e1c37fb0bb7167a
Toby Io <toby.io@outlook.com>
2024-11-06 16:11:20 -0500

    Added: docstring for func

--- Q4/DDIM.py
+++ Q4/DDIM.py
@@ -9,6 +9,15 @@ pipeline = DDIMPipeline.from_pretrained("google/ddpm-celebahq-256")
 generator = torch.Generator().manual_seed(88)
 
 def sample_and_plot(steps):
+    """
+    Generates and plots an image using a diffusion model, saving the final image as a .png file.
+
+    Args:
+        steps (int): The number of inference steps to use in the diffusion process.
+
+    Example:
+        sample_and_plot(steps=50)
+    """
     with torch.no_grad():
         generated_output = pipeline(generator=generator, num_inference_steps=steps, output_type="np")
         generated_image = generated_output.images[0]
